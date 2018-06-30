0b91b1d5fe77b5cf8517d0582afa3d6c92a35b57
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-01 01:27:39 +0200

    Initial code check in

---
+++ src/ChildProcessPoolsCollector.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Inspector\ChildProcessPools;
+
+use WyriHaximus\React\Inspector\CollectorInterface;
+use WyriHaximus\React\Inspector\Metric;
+use WyriHaximus\React\ProcessOutcome;
+use function WyriHaximus\React\childProcessPromise;
+
+final class ChildProcessPoolsCollector implements CollectorInterface
+{
+}
---
+++ tests/ChildProcessPoolsCollectorTest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\React\Tests\Inspector\CPUUsage;
+
+use WyriHaximus\React\Inspector\Metric;
+
+final class ChildProcessPoolsCollectorTest extends TestCase
+{
+}
