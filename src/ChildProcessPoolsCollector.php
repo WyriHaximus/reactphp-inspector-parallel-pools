2c36d5e239528a9364aab77f80c4b925606f8549
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-20 17:28:29 +0200

    Initial implementation

--- src/ChildProcessPoolsCollector.php
+++ src/ChildProcessPoolsCollector.php
@@ -2,11 +2,42 @@
 
 namespace WyriHaximus\React\Inspector\ChildProcessPools;
 
+use Rx\ObservableInterface;
+use WyriHaximus\React\ChildProcess\Pool\PoolInterface;
 use WyriHaximus\React\Inspector\CollectorInterface;
 use WyriHaximus\React\Inspector\Metric;
-use WyriHaximus\React\ProcessOutcome;
-use function WyriHaximus\React\childProcessPromise;
+use function ApiClients\Tools\Rx\observableFromArray;
 
 final class ChildProcessPoolsCollector implements CollectorInterface
 {
+    /**
+     * @var PoolInterface[]
+     */
+    private $pools = [];
+
+    public function register(string $key, PoolInterface $pool)
+    {
+        $this->pools[$key] = $pool;
+    }
+
+    public function collect(): ObservableInterface
+    {
+        $metrics = [];
+
+        foreach ($this->pools as $key => $pool) {
+            foreach ($pool->info() as $metric => $value) {
+                $metrics[] = new Metric(
+                    'childprocess.pool.' . $key . '.' . $metric,
+                    $value
+                );
+            }
+        }
+
+        return observableFromArray($metrics);
+    }
+
+    public function cancel(): void
+    {
+        $this->pools = [];
+    }
 }
